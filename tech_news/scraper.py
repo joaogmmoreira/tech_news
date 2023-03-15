e77a6a26ade4350254015026eb8d172d4f67e16a
Joao Moreira <jgmoreira1809@gmail.com>
2023-03-15 09:55:59 -0300

    Req 01 OK

--- tech_news/scraper.py
+++ tech_news/scraper.py
@@ -1,6 +1,17 @@
+import requests
+import time
+
+
 # Requisito 1
 def fetch(url):
-    """código deve vir aqui"""
+    time.sleep(1)
+    try:
+        response = requests.get(url)
+
+        if response.status_code == 200:
+            return response.text
+    except requests.Timeout:
+        return None
 
 
 # Requisito 2
