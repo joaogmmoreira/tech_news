577d02e81561534c10f1cc65a625a90d7c484952
Joao Moreira <jgmoreira1809@gmail.com>
2023-03-17 14:37:14 -0300

    Req 06 OK

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,18 @@
 from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+from tests.assets.news import NEWS
+import pytest
 
 
-def test_reading_plan_group_news():
-    pass
+def test_reading_plan_group_news(mocker):
+    mocker.patch(
+        "tech_news.analyzer.reading_plan.find_news", return_value=NEWS
+        )
+    all_news = ReadingPlanService.group_news_for_available_time(2)
+
+    assert len(all_news["readable"]) == 6
+    assert len(all_news["unreadable"]) == 4
+
+    with pytest.raises(
+        ValueError, match="Valor 'available_time' deve ser maior que zero"
+    ):
+        ReadingPlanService.group_news_for_available_time(0)
